a68617e371cd3cc08f8ef458845158c854bbd14c
Flynn Stacey <flynnstacey@hotmail.com>
2021-04-26 13:10:40 +0100

    update

--- whynow-test/src/components/RadioComponent/RadioSchedule.jsx
+++ whynow-test/src/components/RadioComponent/RadioSchedule.jsx
@@ -20,6 +20,7 @@ export default class RadioSchedule extends React.Component {
 
   render() {
     console.log(this.state.activeToggle, this.state.rowNum);
+
     return (
       <>
         <CustomContainer>
@@ -30,146 +31,152 @@ export default class RadioSchedule extends React.Component {
           </ul>
 
           <h1>Schedule</h1>
-          <StyledTable responsive>
-            <thead>
-              <tr>
-                <th className="d-flex w-30">
-                  <p className="dayDateHeading">
-                    <span className="text-white">TODAY,</span> 10 MARCH
-                  </p>
-                  <p className="pl-2">EARLIER</p>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="tableBodyBorder">
-              <tr>
-                <td className={this.state.rowNum === 1 ? "showTime" : null}>
-                  13:00 - 14:00
-                </td>
-                <td className="showTitle" colSpan="12">
-                  <div className="show-info-box">
-                    <div className="image-container">
-                      <img
-                        src="https://whynow.co.uk/wp-content/uploads/2021/03/Jamie-Rodigan-Kabaka-Pyramid-Kaylee-Kay-site-720x720.jpg"
-                        alt="show profile"
-                      />
-                      <button
-                        id="1"
-                        className="playBtnCircle"
-                        onClick={(e) => this.handleClick(e)}
-                      >
-                        <div className="playBtnArrow"></div>
-                      </button>
+          <div className="table-responsive-sm">
+            <StyledTable responsive>
+              <thead>
+                <tr>
+                  <th className="d-flex">
+                    <p className="dayDateHeading">
+                      <span className="text-white">TODAY,</span> 10 MARCH
+                    </p>
+                    <p className="pl-2">EARLIER</p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="tableBodyBorder">
+                <tr>
+                  <td>
+                    <div
+                      className={this.state.rowNum === 1 ? "showTime" : null}
+                    >
+                      13:00 - 14:00
                     </div>
-                    <div>
-                      <h3>Jamie Rodigan's 'SoundSystem Show'</h3>
-                      <p>w/ Kabaka Pyramid + Kaylee Kay</p>
+                  </td>
+                  <td className="showTitle" colSpan="12">
+                    <div className="show-info-box">
+                      <div className="image-container">
+                        <img
+                          src="https://whynow.co.uk/wp-content/uploads/2021/03/Jamie-Rodigan-Kabaka-Pyramid-Kaylee-Kay-site-720x720.jpg"
+                          alt="show profile"
+                        />
+                        <button
+                          id="1"
+                          className="playBtnCircle"
+                          onClick={(e) => this.handleClick(e)}
+                        >
+                          <div className="playBtnArrow"></div>
+                        </button>
+                      </div>
+                      <div className="line-clamp">
+                        <h3>Jamie Rodigan's 'SoundSystem Show'</h3>
+                        <p>w/ Kabaka Pyramid + Kaylee Kay</p>
+                      </div>
                     </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className={this.state.rowNum === 2 ? "showTime" : null}>
-                  15:00 - 16:00
-                </td>
-                <td className="showTitle" colSpan="12">
-                  <div className="show-info-box">
-                    <div className="image-container">
-                      <img
-                        src="https://whynow.co.uk/wp-content/uploads/2021/03/Laurie-Bryan-SQ-site-720x720.jpg"
-                        alt="show profile"
-                      />
-                      <button
-                        id="2"
-                        className="playBtnCircle"
-                        onClick={this.handleClick}
-                      >
-                        <div className="playBtnArrow"></div>
-                      </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td className={this.state.rowNum === 2 ? "showTime" : null}>
+                    15:00 - 16:00
+                  </td>
+                  <td className="showTitle" colSpan="12">
+                    <div className="show-info-box">
+                      <div className="image-container">
+                        <img
+                          src="https://whynow.co.uk/wp-content/uploads/2021/03/Laurie-Bryan-SQ-site-720x720.jpg"
+                          alt="show profile"
+                        />
+                        <button
+                          id="2"
+                          className="playBtnCircle"
+                          onClick={this.handleClick}
+                        >
+                          <div className="playBtnArrow"></div>
+                        </button>
+                      </div>
+                      <div className="line-clamp">
+                        <h3>Laurie's Liquid Hour</h3>
+                        <p>w/ Bryan Gee</p>
+                      </div>
                     </div>
-                    <div>
-                      <h3>Laurie's Liquid Hour</h3>
-                      <p>w/ Bryan Gee</p>
+                  </td>
+                </tr>
+              </tbody>
+              <thead>
+                <tr>
+                  <th className="d-flex">
+                    <p className="dayDateHeading">
+                      <span className="text-white">THURSDAY,</span> 11 MARCH
+                    </p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="tableBodyBorder">
+                <tr>
+                  <td className={this.state.rowNum === 3 ? "showTime" : null}>
+                    17:00 - 18:30
+                  </td>
+                  <td className="showTitle" colSpan="12">
+                    <div className="show-info-box">
+                      <div className="image-container">
+                        <img
+                          src="https://whynow.co.uk/wp-content/uploads/2021/03/Screenshot-2021-03-01-at-12.12.55-720x720.png"
+                          alt="show profile"
+                        />
+                        <button
+                          id="3"
+                          className="playBtnCircle"
+                          onClick={this.handleClick}
+                        >
+                          <div className="playBtnArrow"></div>
+                        </button>
+                      </div>
+                      <div className="line-clamp">
+                        <h3>The Jaguar Skills Show</h3>
+                        <p>w/ DJ Yoda</p>
+                      </div>
                     </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-            <thead>
-              <tr>
-                <th className="d-flex w-30">
-                  <p className="dayDateHeading">
-                    <span className="text-white">THURSDAY,</span> 11 MARCH
-                  </p>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="tableBodyBorder">
-              <tr>
-                <td className={this.state.rowNum === 3 ? "showTime" : null}>
-                  17:00 - 18:30
-                </td>
-                <td className="showTitle" colSpan="12">
-                  <div className="show-info-box">
-                    <div className="image-container">
-                      <img
-                        src="https://whynow.co.uk/wp-content/uploads/2021/03/Screenshot-2021-03-01-at-12.12.55-720x720.png"
-                        alt="show profile"
-                      />
-                      <button
-                        id="3"
-                        className="playBtnCircle"
-                        onClick={this.handleClick}
-                      >
-                        <div className="playBtnArrow"></div>
-                      </button>
+                  </td>
+                </tr>
+              </tbody>
+              <thead>
+                <tr>
+                  <th className="d-flex">
+                    <p className="dayDateHeading">
+                      <span className="text-white">FRIDAY,</span> 12 MARCH
+                    </p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="tableBodyBorder">
+                <tr>
+                  <td className={this.state.rowNum === 4 ? "showTime" : null}>
+                    20:00 - 21:00
+                  </td>
+                  <td className="showTitle" colSpan="12">
+                    <div className="show-info-box">
+                      <div className="image-container">
+                        <img
+                          src="https://whynow.co.uk/wp-content/uploads/2021/03/lester-Lloyd-SQ-site-720x720.jpg"
+                          alt="show profile"
+                        />
+                        <button
+                          id="4"
+                          className="playBtnCircle"
+                          onClick={this.handleClick}
+                        >
+                          <div className="playBtnArrow"></div>
+                        </button>
+                      </div>
+                      <div className="line-clamp">
+                        <h3>Lester Lloyd 'The Morale Booster'</h3>
+                        <p>w/ Graham Sayle</p>
+                      </div>
                     </div>
-                    <div>
-                      <h3>The Jaguar Skills Show</h3>
-                      <p>w/ DJ Yoda</p>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-            <thead>
-              <tr>
-                <th className="d-flex w-30">
-                  <p className="dayDateHeading">
-                    <span className="text-white">FRIDAY,</span> 12 MARCH
-                  </p>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="tableBodyBorder">
-              <tr>
-                <td className={this.state.rowNum === 4 ? "showTime" : null}>
-                  20:00 - 21:00
-                </td>
-                <td className="showTitle" colSpan="12">
-                  <div className="show-info-box">
-                    <div className="image-container">
-                      <img
-                        src="https://whynow.co.uk/wp-content/uploads/2021/03/lester-Lloyd-SQ-site-720x720.jpg"
-                        alt="show profile"
-                      />
-                      <button
-                        id="4"
-                        className="playBtnCircle"
-                        onClick={this.handleClick}
-                      >
-                        <div className="playBtnArrow"></div>
-                      </button>
-                    </div>
-                    <div>
-                      <h3>Lester Lloyd 'The Morale Booster'</h3>
-                      <p>w/ Graham Sayle</p>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </StyledTable>
+                  </td>
+                </tr>
+              </tbody>
+            </StyledTable>
+          </div>
         </CustomContainer>
       </>
     );
@@ -225,6 +232,10 @@ const StyledTable = styled(Table)`
     border-top: 2px solid white;
   }
 
+  & .showTitle {
+    min-width: 320px;
+  }
+
   & td {
     padding: 2rem 0 0.5rem 0;
     color: #b2b2b2;
@@ -237,6 +248,14 @@ const StyledTable = styled(Table)`
     }
     .show-info-box {
       display: flex;
+      min-width: 320px;
+    }
+
+    .line-clamp {
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
     }
   }
 
